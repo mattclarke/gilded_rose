6d93d572e99ff35f6a4534696f69cdf6db686599
Kenan Muric <kenan.muric@ess.eu>
2023-01-26 10:31:01 +0100

    yeah

--- test_example.py
+++ test_example.py
@@ -3,8 +3,14 @@ from world import World
 
 def test_create_world_and_check_is_empty():
     world = World()
+    assert world.isempty()
 
-    assert len(world.cells) == 0
+
+def test_add_one_cell_to_the_world():
+    world = World()
+    world.add_cell((0, 0))
+    assert not world.isempty()
+
 
 #def test_create_cell_and_check_exists():
 #    cell = (1, 1)
--- world.py
+++ world.py
@@ -1,3 +1,9 @@
 class World:
     def __init__(self):
         self.cells = set()
+
+    def add_cell(self, cell):
+        self.cells.add(cell)
+
+    def isempty(self):
+        return len(self.cells) == 0
