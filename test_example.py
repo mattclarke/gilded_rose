570de4a478cc7a6229eadc7812dcca793e8cb141
Matt Clarke <matt.clarke@esss.se>
2023-01-26 10:15:11 +0100

    initial test

--- test_example.py
+++ test_example.py
@@ -1,3 +1,14 @@
+
+
+
 def test_two_plus_two():
     assert 2 + 2 == 4
 
+def test_create_world():
+    world = World()
+
+    assert world.cells.empty()
+
+#def test_create_cell_and_check_exists():
+#    cell = (1, 1)
+  
