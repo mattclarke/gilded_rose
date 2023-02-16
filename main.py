acd0e46b87982f085e92ac6df1090a7e5743f99b
Jonas Petersson <jonas.petersson@ess.eu>
2023-02-16 10:33:33 +0100

    testing

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
+class Collector:
+    def __init__(self):
+        self.tests = []
+        self.num_passed = 0
+
+    def add_test(self, test):
+        self.tests.append(test)
+
+    def run_tests(self):
+        for test in self.tests:
+            test.run()
+            if test.passed:
+                self.num_passed += 1
+
 class Test:
     def __init__(self, function):
         self.was_run = False
@@ -32,8 +46,16 @@ def test_test_is_run():
     test.run()
     assert test.was_run == True
 
+def test_can_collect_results():
+    collector = Collector()
+    collector.add_test(Test(lambda: 123))
+    collector.run_tests()
+    assert collector.num_passed == 1
+    assert collector.num_runs == 1
+
 
 if __name__ == "__main__":
     test_test_is_run()
     test_test_passes()
     test_test_fails()
+    test_can_collect_results()
