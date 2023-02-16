e92dc460fd6de2174727a8c70d45012f023228ee
stefanosathanasopoul <stefanos.athanasopoulos@ess.eu>
2023-02-16 10:47:42 +0100

    bah

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class Collector:
         self.tests = []
         self.num_passed = 0
         self.num_runs = 0
+        self.results = ""
+
+    def get_results(self):
+        return self.results
 
     def add_test(self, test):
         self.tests.append(test)
@@ -14,6 +18,7 @@ class Collector:
             if test.passed:
                 self.num_passed += 1
             self.num_runs += 1
+        self.results = f"{self.num_passed} of {self.num_runs}"
 
 class Test:
     def __init__(self, function):
@@ -71,9 +76,17 @@ def test_collector_can_print_results():
     results = collector.get_results()
     assert results == "1 of 1"
 
+def test_collector_can_list_failed_tests():
+    collector = Collector()
+    def _fails():
+        raise RuntimeError()
+    collector.add_test(Test(_fails))
+    collector.run_tests()
+
 if __name__ == "__main__":
     test_test_is_run()
     test_test_passes()
     test_test_fails()
     test_can_collect_one_fail()
     test_can_collect_one_pass()
+    test_collector_can_print_results()
