c4be03b9f12eb82527bd86fc944280f6ff4a3db6
stefanosathanasopoul <stefanos.athanasopoulos@ess.eu>
2023-09-01 11:29:47 +0200

    Bye Felicia

--- tests/test_ohce.py
+++ tests/test_ohce.py
@@ -1,20 +1,15 @@
 from datetime import datetime
 
 
-NAME = None
-
-
 def get_time():
     return datetime.now().hour
 
 
-def get_response(name, prompt, time_function=get_time):
-    
+def get_response(name, prompt="", time_function=get_time):
 
-    global NAME
-    if name == "Stop!":
-        return f"Adios {NAME}"
-    NAME = name
+
+    if prompt == "Stop!":
+        return f"Adios {name}"
     current_hour = time_function()
     if current_hour >= 20 or current_hour < 6:
         return f"¡Buenas noches {name}!"
@@ -25,25 +20,25 @@ def get_response(name, prompt, time_function=get_time):
 
 
 def test_between_20_and_6_returns_buenas_noches():
-    response = get_response("Daniel", lambda: 20)
+    response = get_response("Daniel", time_function=lambda: 20)
 
     assert response == "¡Buenas noches Daniel!"
 
 
 def test_between_6_and_12_returns_buenos_dias():
-    response = get_response("Federica", lambda: 6)
+    response = get_response("Federica", time_function=lambda: 6)
 
     assert response == "¡Buenos días Federica!"
 
 
 def test_says_buenas_tardes_daniel():
-    response = get_response("Daniel", lambda: 12)
+    response = get_response("Daniel", time_function=lambda: 12)
 
     assert response == "¡Buenas tardes Daniel!"
 
 
 def test_on_stop_says_adios():
-    _ = get_response("Daniel", lambda: 12)
-    response = get_response("Stop!", lambda: 12)
+    # _ = get_response("Daniel", lambda: 12)
+    response = get_response("Daniel", "Stop!", time_function=lambda: 12)
 
     assert response == "Adios Daniel"
