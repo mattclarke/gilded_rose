1a83dc2d74a865971713adb066308d566b35d040
Jonas Petersson <jonas.petersson@ess.eu>
2023-09-01 11:00:22 +0200

    s

--- tests/test_ohce.py
+++ tests/test_ohce.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 
-def get_time():
-    pass
 
-def get_response(name):
+def get_time():
+    return datetime.now().hour
+
+
+def get_response(name, time_function=get_time):
+    current_hour = time_function()
+    if 6 < current_hour >= 20:
+        return f"¡Buenas noches {name}!"
     return f"¡Buenos días {name}!"
 
 
 def test_between_20_and_6_returns_buenas_noches():
 
-    response = get_response("Daniel")
+    response = get_response("Daniel", lambda: 20)
 
     assert response == "¡Buenas noches Daniel!"
 
