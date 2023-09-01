e243b9be969c6ba3b9888c41d0047b514f2a7c90
Daniel Cacabelos <daniel.cacabelos@ess.eu>
2023-09-01 11:17:57 +0200

    Yolo

--- tests/test_ohce.py
+++ tests/test_ohce.py
@@ -16,21 +16,25 @@ def get_response(name, time_function=get_time):
 
 
 def test_between_20_and_6_returns_buenas_noches():
-
     response = get_response("Daniel", lambda: 20)
 
     assert response == "¡Buenas noches Daniel!"
 
 
 def test_between_6_and_12_returns_buenos_dias():
-
     response = get_response("Federica", lambda: 6)
 
     assert response == "¡Buenos días Federica!"
 
 
 def test_says_buenas_tardes_daniel():
-
     response = get_response("Daniel", lambda: 12)
 
     assert response == "¡Buenas tardes Daniel!"
+
+
+def test_on_stop_says_adios():
+    _ = get_response("Daniel", lambda: 12)
+    response = get_response("Stop!", lambda: 12)
+
+    assert response == "Adios Daniel"
