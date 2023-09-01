f49a23abc5eaff22e3f2bd045e278c5e8b1de271
Daniel Cacabelos <daniel.cacabelos@ess.eu>
2023-09-01 10:53:13 +0200

    Yeah

--- tests/test_ohce.py
+++ tests/test_ohce.py
@@ -1,8 +1,27 @@
-def get_response(name):
+from datetime import datetime
+
+def get_time():
     pass
 
+def get_response(name):
+    return f"¡Buenos días {name}!"
 
-def test_says_buenos_dias():
+
+def test_between_20_and_6_returns_buenas_noches():
+
+    response = get_response("Daniel")
+
+    assert response == "¡Buenas noches Daniel!"
+
+
+def test_says_buenos_dias_daniel():
+
+    response = get_response("Daniel")
+
+    assert response == "¡Buenos días Daniel!"
+
+
+def test_says_buenos_dias_pedro():
 
     response = get_response("Pedro")
 
