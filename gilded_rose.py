91d479274a24af00851ada2690a7b209748c74a5
Jonas Petersson <jonas.petersson@ess.eu>
2024-10-11 13:40:40 +0200

    refacto expired

--- gilded_rose.py
+++ gilded_rose.py
@@ -29,15 +29,25 @@ class GildedRose(object):
                 # brie
                 if item.quality < 50:
                     item.quality += 1
+
             item.sell_in -= 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
+                if item.name == "Aged Brie":
+                    # Brie
+                    if item.quality < 50:
+                        item.quality += 1
+
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
+
+                elif item.name != "Aged Brie":
+                    # Normal, backstage
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        # Normal
                         if item.quality > 0:
                             item.quality -= 1
                     else:
+                        # backstage
                         item.quality = 0
-                else:
-                    if item.quality < 50:
-                        item.quality += 1
 
