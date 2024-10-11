810365c9f3e1ac65f7af0ebd02897574445de822
Lais Pessine do Carmo <lais.pessine@ess.eu>
2024-10-11 13:35:32 +0200

    Clean up backstage passes; -=1

--- gilded_rose.py
+++ gilded_rose.py
@@ -16,27 +16,20 @@ class GildedRose(object):
                 if item.quality > 0:
                     item.quality -= 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                # backstage passes
                 if item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality += 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality += 1
             else:
-                # brie or backstagepasses
+                # brie
                 if item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
-            item.sell_in = item.sell_in - 1
+            item.sell_in -= 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
