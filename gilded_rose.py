cf98ce04c266bfb4de28b8e405cab4a0cbb70c95
Matt Clarke <matt.clarke@ess.eu>
2024-10-11 13:30:23 +0200

    duplication

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,9 +12,21 @@ class GildedRose(object):
                 continue
 
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                # this is normal items
                 if item.quality > 0:
                     item.quality -= 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality += 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality += 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality += 1
             else:
+                # brie or backstagepasses
                 if item.quality < 50:
                     item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
