1395ac086228333b87811a6a1c6d25ac45b10d7b
Jonas Petersson <jonas.petersson@ess.eu>
2024-10-11 13:20:14 +0200

    simplify additions

--- gilded_rose.py
+++ gilded_rose.py
@@ -14,14 +14,14 @@ class GildedRose(object):
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality +=  1
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality +=  1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -34,5 +34,5 @@ class GildedRose(object):
                         item.quality = 0
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality +=  1
 
